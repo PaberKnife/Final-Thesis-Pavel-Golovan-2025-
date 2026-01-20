8674bc0714019ed961ee98b25557504c536e9d66
Pavel Golovan <tdjmpdb5457@gmail.com>
2026-01-20 22:55:49 +0200

    Bug fix

---
+++ VRChat World/Assets/Portals/TeleportScriptUdonSharp.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TeleportScriptUdonSharp : UdonSharpBehaviour
+{
+
+    public Transform TargetLocation;
+
+    public override void Interact()
+    {
+        VRCPlayerApi localPlayer = Networking.LocalPlayer;
+        if (localPlayer == null || TargetLocation == null) return;
+
+        localPlayer.TeleportTo(TargetLocation.position, TargetLocation.rotation);
+    }
+}
