c5b1063d07db4c8c228c3e96f952e44f90800225
Pavel Golovan <tdjmpdb5457@gmail.com>
2026-01-24 16:54:48 +0200

    Big update + private release

---
+++ VRChat World/Assets/Mirrors/MirrorsScriptUdonSharp.cs
@@ -0,0 +1,65 @@
+﻿
+using JetBrains.Annotations;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MirrorsScriptUdonSharp : UdonSharpBehaviour
+{
+
+    public GameObject[] mirrors;
+
+    public void MirrorsTurnOff()
+    {
+        for(int i = 0; i < mirrors.Length; i++)
+        {
+            mirrors[i].SetActive(false);
+        }
+    }
+
+    public void MirrorsTurnOnLow()
+    {
+        for (int i = 0; i < mirrors.Length; i++)
+        {
+            if (i == 0)
+            {
+                mirrors[i].SetActive(true);
+            }
+            else
+            {
+                mirrors[i].SetActive(false);
+            }
+        }
+    }
+
+    public void MirrorsTurnOnMedium()
+    {
+        for (int i = 0; i < mirrors.Length; i++)
+        {
+            if (i == 1)
+            {
+                mirrors[i].SetActive(true);
+            }
+            else
+            {
+                mirrors[i].SetActive(false);
+            }
+        }
+    }
+
+    public void MirrorsTurnOnHigh()
+    {
+        for (int i = 0; i < mirrors.Length; i++)
+        {
+            if (i == 2)
+            {
+                mirrors[i].SetActive(true);
+            }
+            else
+            {
+                mirrors[i].SetActive(false);
+            }
+        }
+    }
+}
---
+++ VRChat World/Assets/ProximityScriptUdonSharp.cs
@@ -0,0 +1,25 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ProximityScriptUdonSharp : UdonSharpBehaviour
+{
+
+    public GameObject affectedObject;
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (!player.isLocal) return;
+
+        affectedObject.SetActive(true);
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        if (!player.isLocal) return;
+
+        affectedObject.SetActive(false);
+    }
+}
