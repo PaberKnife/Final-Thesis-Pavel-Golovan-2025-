e8927ae4a1c0dfa5721e6228e31534279a82b632
Pavel Golovan <tdjmpdb5457@gmail.com>
2026-01-26 21:14:58 +0200

    IOS release, information panels + bug fixes

---
+++ VRChat World/Assets/InfoBoxes/InfoBoxes_Script.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class InfoBoxes_Script : UdonSharpBehaviour
+{
+
+    public GameObject affectedObject;
+
+    public override void Interact()
+    {
+        if (affectedObject.activeSelf == true)
+        {
+            affectedObject.SetActive(false);
+        }
+        else
+        {
+            affectedObject.SetActive(true);
+        }
+    }
+}
